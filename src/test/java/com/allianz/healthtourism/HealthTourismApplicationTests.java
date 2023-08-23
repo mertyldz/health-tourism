30a00e35ef7aed9fc2449720695d8033965405ef
Mert Yıldız <yzmert3@gmail.com>
2023-08-23 18:43:22 +0300

    create project and add dependencies

---
+++ src/test/java/com/allianz/healthtourism/HealthTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianz.healthtourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTourismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
