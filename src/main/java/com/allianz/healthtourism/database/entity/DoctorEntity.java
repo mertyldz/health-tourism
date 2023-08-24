fac31f05e47bd884f0b6d51ae50fbfa79711ca0f
Mert Yıldız <yzmert3@gmail.com>
2023-08-24 18:40:03 +0300

    add doctor entity

---
+++ src/main/java/com/allianz/healthtourism/database/entity/DoctorEntity.java
@@ -0,0 +1,45 @@
+package com.allianz.healthtourism.database.entity;
+
+import com.allianz.healthtourism.model.enums.DoctorProfessionEnum;
+import com.allianz.healthtourism.model.enums.GenderEnum;
+import com.allianz.healthtourism.utils.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "patient")
+public class DoctorEntity extends BaseEntity {
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate birthDate;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String email;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private DoctorProfessionEnum profession;
+
+    // Add hospital info
+
+}
---
+++ src/main/java/com/allianz/healthtourism/model/enums/DoctorProfessionEnum.java
@@ -0,0 +1,24 @@
+package com.allianz.healthtourism.model.enums;
+
+public enum DoctorProfessionEnum {
+    OPHTHALMOLOGY,
+    ENT,
+    DENTISTRY,
+    CARDIOLOGY,
+    DERMATOLOGY,
+    NEUROLOGY,
+    ORTHOPEDICS,
+    PEDIATRICS,
+    PSYCHIATRY,
+    UROLOGY,
+    GYNECOLOGY,
+    GASTROENTEROLOGY,
+    ENDOCRINOLOGY,
+    HEMATOLOGY,
+    INFECTIOUS_DISEASE,
+    NEPHROLOGY,
+    ONCOLOGY,
+    PULMONOLOGY,
+    RHEUMATOLOGY,
+    PLASTIC_SURGERY;
+}
