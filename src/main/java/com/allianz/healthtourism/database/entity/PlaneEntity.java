4d87921ee20b8cc654f14d2998f86cc71fd6f43d
Mert Yıldız <yzmert3@gmail.com>
2023-08-24 22:41:00 +0300

    add basic plane entity

---
+++ src/main/java/com/allianz/healthtourism/database/entity/PlaneEntity.java
@@ -0,0 +1,29 @@
+package com.allianz.healthtourism.database.entity;
+
+import com.allianz.healthtourism.utils.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class PlaneEntity extends BaseEntity {
+    @Column
+    private String flightNo;
+    @Column
+    private String fromPlace;
+    @Column
+    private String toPlace;
+
+    // Check is it ok
+    @Column
+    private LocalTime departureTime;
+
+    // add relation with reservation ....
+}
