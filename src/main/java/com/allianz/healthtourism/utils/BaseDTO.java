f79d666678dda625ee51b7c1a65f18abd8ee5c25
Mert Yıldız <yzmert3@gmail.com>
2023-08-26 15:50:33 +0300

    add base dto

---
+++ src/main/java/com/allianz/healthtourism/utils/BaseDTO.java
@@ -0,0 +1,19 @@
+package com.allianz.healthtourism.utils;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class BaseDTO {
+    private Long id;
+    private UUID uuid;
+
+    @JsonIgnore
+    private Date creationDate;
+
+    @JsonIgnore
+    private Date updatedDate;
+}
