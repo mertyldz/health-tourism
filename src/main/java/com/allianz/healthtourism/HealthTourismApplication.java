1de43d93136c090499aa69f343a00d4cebc0ef2d
Mert Yıldız <yzmert3@gmail.com>
2023-08-26 15:25:49 +0300

    test reservation relations

--- src/main/java/com/allianz/healthtourism/HealthTourismApplication.java
+++ src/main/java/com/allianz/healthtourism/HealthTourismApplication.java
@@ -4,6 +4,7 @@ import com.allianz.healthtourism.database.entity.*;
 import com.allianz.healthtourism.database.repository.*;
 import com.allianz.healthtourism.model.enums.GenderEnum;
 import com.allianz.healthtourism.model.enums.ProfessionEnum;
+import com.allianz.healthtourism.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -35,11 +36,12 @@ public class HealthTourismApplication implements CommandLineRunner {
     HotelRepository hotelRepository;
     @Autowired
     ExaminationRepository examinationRepository;
+    @Autowired
+    ReservationService reservationService;
 
     public static void main(String[] args) {
         SpringApplication.run(HealthTourismApplication.class, args);
     }
-
     @Override
     public void run(String... args) throws Exception {
 
@@ -48,63 +50,82 @@ public class HealthTourismApplication implements CommandLineRunner {
         generateDoctor();
         generateHospital();
         addDoctorToHospital();
+        generatePlane();
+        generateFlight();
+        addPlaneToFlight();
+        generateHotel();
+        generateReservation();
+        reservationService.addRelationsToReservation();
+        generateExamination();
+        addPatientToExamination();
+
+    }
 
 
-//
-//        // Create plane
-//        PlaneEntity plane = new PlaneEntity();
-//        plane.setAirline("THY");
-//        plane.setModel("Boeing");
-//        plane.setDepartureTime(LocalDateTime.of(2023, Month.JULY, 29,14,15));
-//        plane.setDepartureCity("Berlin");
-//        plane.setArrivalCity("İstanbul");
-//        planeRepository.save(plane);
-//
-//        // Create Flight
-//        FlightEntity flight = new FlightEntity();
-//        flight.setFlightNo("AZXC1234");
-//        flightRepository.save(flight);
-//
-//        // Add flight-plane relation
-//        PlaneEntity plane1 = planeRepository.findById(1L).orElse(null);
-//        FlightEntity flight1 = flightRepository.findById(1L).orElse(null);
-//        if (plane1 != null && flight1 != null) {
-//            flight1.setPlane(plane1);
-//            flightRepository.save(flight1);
-//        }
-//
-//        System.out.println(plane);
-//
-//        // Create Hotel
-//        HotelEntity hotel = new HotelEntity();
-//        hotel.setHotelName("Ramada");
-//        hotel.setCity("İstanbul");
-//        hotel.setAddress("Nişantaşı");
-//        hotel.setTotalCapacity(500);
-//        hotel.setTakenCapacity(100);
-//        hotelRepository.save(hotel);
-//
-//        // Create reservation
-//        ReservationEntity reservation = new ReservationEntity();
-//        reservation.setReservationDate(LocalDate.of(2023, Month.APRIL, 25));
-//        reservation.setTreatmentProfession(ProfessionEnum.ENDOCRINOLOGY);
-//        reservationRepository.save(reservation);
-//
-//        // Add relations to reservation
-//        ReservationEntity reservation1 = reservationRepository.findById(1L).orElse(null);
-//        reservation1.setDoctor(doctorRepository.findById(1L).orElse(null));
-//        reservation1.setHotel(hotelRepository.findById(1L).orElse(null));
-//        reservation1.setFlight(flightRepository.findById(1L).orElse(null));
-//        reservation1.setPatient(patientRepository.findById(1L).orElse(null));
-//        reservation1.setHospital(hospitalRepository.findById(1L).orElse(null));
-//        reservationRepository.save(reservation1);
 
+    private void generateExamination() {
+        // Create Examination
+        ExaminationEntity examination = new ExaminationEntity();
+        examination.setDiagnosis("Hair loss");
+        examination.setTreatmentPrescription("Hair transplant");
+        examination.setExaminationDate(LocalDate.of(2023, Month.JULY, 20));
+        examinationRepository.save(examination);
+    }
 
-//        // Create Examination
-//        ExaminationEntity examination = new ExaminationEntity();
-//        examination.setDiagnosis("Hair loss");
-//        examination.setTreatmentPrescription("Hair transplant");
-//        examination.setExaminationDate(LocalDate.of(2023, Month.JULY, 20));
+    private void addPatientToExamination() {
+        ExaminationEntity examination = examinationRepository.findById(1L).orElse(null);
+        PatientEntity patient = patientRepository.findById(1L).orElse(null);
+        patient.setExamination(examination);
+        patientRepository.save(patient);
+
+    }
+
+    private void generateReservation() {
+        // Create reservation
+        ReservationEntity reservation = new ReservationEntity();
+        reservation.setReservationDate(LocalDate.of(2023, Month.APRIL, 25));
+        reservation.setTreatmentProfession(ProfessionEnum.ENDOCRINOLOGY);
+        reservationRepository.save(reservation);
+    }
+
+    private void generateHotel() {
+        // Create Hotel
+        HotelEntity hotel = new HotelEntity();
+        hotel.setHotelName("Ramada");
+        hotel.setCity("İstanbul");
+        hotel.setAddress("Nişantaşı");
+        hotel.setTotalCapacity(500);
+        hotel.setTakenCapacity(100);
+        hotelRepository.save(hotel);
+    }
+
+    private void addPlaneToFlight() {
+        // Add flight-plane relation
+        PlaneEntity plane1 = planeRepository.findById(1L).orElse(null);
+        FlightEntity flight1 = flightRepository.findById(1L).orElse(null);
+        if (plane1 != null && flight1 != null) {
+            flight1.setPlane(plane1);
+            flightRepository.save(flight1);
+        }
+        System.out.println(plane1);
+    }
+
+    private void generateFlight() {
+        // Create Flight
+        FlightEntity flight = new FlightEntity();
+        flight.setFlightNo("AZXC1234");
+        flightRepository.save(flight);
+    }
+
+    private void generatePlane() {
+        // Create plane
+        PlaneEntity plane = new PlaneEntity();
+        plane.setAirline("THY");
+        plane.setModel("Boeing");
+        plane.setDepartureTime(LocalDateTime.of(2023, Month.JULY, 29, 14, 15));
+        plane.setDepartureCity("Berlin");
+        plane.setArrivalCity("İstanbul");
+        planeRepository.save(plane);
     }
 
     private void addDoctorToHospital() {
