9356d702e80106fff60b7518ee844f8ef6a4e739
Mert Yıldız <yzmert3@gmail.com>
2023-08-26 15:57:04 +0300

    add IBaseMapper,PageDTO, BaseRequestDTO

---
+++ src/main/java/com/allianz/healthtourism/utils/BaseRequestDTO.java
@@ -0,0 +1,10 @@
+package com.allianz.healthtourism.utils;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class BaseRequestDTO {
+    private UUID uuid;
+}
---
+++ src/main/java/com/allianz/healthtourism/utils/IBaseMapper.java
@@ -0,0 +1,28 @@
+package com.allianz.healthtourism.utils;
+
+import com.allianz.healthtourism.model.requestDTO.PageDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface IBaseMapper<
+        DTO extends BaseDTO,
+        Entity extends BaseEntity,
+        RequestDTO extends BaseRequestDTO> {
+
+    DTO entityToDTO(Entity entity);
+
+    Entity dtoToEntity(DTO dto);
+
+    List<DTO> entityListToDTOList(List<Entity> entityList);
+
+    List<Entity> dtoListTOEntityList(List<DTO> dtoList);
+
+    Entity requestDTOToEntity(RequestDTO dto);
+
+    List<Entity> requestDtoListTOEntityList(List<RequestDTO> dtoList);
+
+    Entity requestDtoToExistEntity(Entity entity, RequestDTO requestDTO);
+
+    PageDTO<DTO> pageEntityToPageDTO(Page<Entity> entityPage);
+}
