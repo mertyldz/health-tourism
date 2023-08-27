6f2c4cece330a928e6b5a26d74fd46391fff033d
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 22:34:16 +0300

    add reservation controller

---
+++ src/main/java/com/allianz/healthtourism/controller/ReservationController.java
@@ -0,0 +1,34 @@
+package com.allianz.healthtourism.controller;
+
+import com.allianz.healthtourism.database.entity.ReservationEntity;
+import com.allianz.healthtourism.database.repository.ReservationRepository;
+import com.allianz.healthtourism.database.specification.ReservationSpecification;
+import com.allianz.healthtourism.mapper.ReservationMapper;
+import com.allianz.healthtourism.model.requestDTO.ReservationRequestDTO;
+import com.allianz.healthtourism.model.responseDTO.ReservationDTO;
+import com.allianz.healthtourism.service.ReservationService;
+import com.allianz.healthtourism.utils.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hospital")
+public class ReservationController extends BaseController<
+        ReservationEntity,
+        ReservationDTO,
+        ReservationRequestDTO,
+        ReservationMapper,
+        ReservationRepository,
+        ReservationSpecification,
+        ReservationService> {
+
+    @Autowired
+    ReservationService reservationService;
+
+    @Override
+    protected ReservationService getService() {
+        return reservationService;
+    }
+
+}
