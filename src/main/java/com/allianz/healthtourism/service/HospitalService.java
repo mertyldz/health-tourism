e05b5c583d8306ef2f33e58ede857927daae890e
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 13:21:34 +0300

    delete unused imports

--- src/main/java/com/allianz/healthtourism/service/HospitalService.java
+++ src/main/java/com/allianz/healthtourism/service/HospitalService.java
@@ -1,16 +1,10 @@
 package com.allianz.healthtourism.service;
 
-import com.allianz.healthtourism.database.entity.FlightEntity;
 import com.allianz.healthtourism.database.entity.HospitalEntity;
-import com.allianz.healthtourism.database.repository.FlightRepository;
 import com.allianz.healthtourism.database.repository.HospitalRepository;
-import com.allianz.healthtourism.database.specification.FlightSpecification;
 import com.allianz.healthtourism.database.specification.HospitalSpecification;
-import com.allianz.healthtourism.mapper.FlightMapper;
 import com.allianz.healthtourism.mapper.HospitalMapper;
-import com.allianz.healthtourism.model.requestDTO.FlightRequestDTO;
 import com.allianz.healthtourism.model.requestDTO.HospitalRequestDTO;
-import com.allianz.healthtourism.model.responseDTO.FlightDTO;
 import com.allianz.healthtourism.model.responseDTO.HospitalDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
