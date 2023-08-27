3626feba103f771027a23734a26245d47835e8b7
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 22:33:17 +0300

    add plane controller

---
+++ src/main/java/com/allianz/healthtourism/controller/PlaneController.java
@@ -0,0 +1,35 @@
+package com.allianz.healthtourism.controller;
+
+import com.allianz.healthtourism.database.entity.PlaneEntity;
+import com.allianz.healthtourism.database.repository.PlaneRepository;
+import com.allianz.healthtourism.database.specification.PlaneSpecification;
+import com.allianz.healthtourism.mapper.PlaneMapper;
+import com.allianz.healthtourism.model.requestDTO.PlaneRequestDTO;
+import com.allianz.healthtourism.model.responseDTO.PlaneDTO;
+import com.allianz.healthtourism.service.PatientService;
+import com.allianz.healthtourism.service.PlaneService;
+import com.allianz.healthtourism.utils.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hospital")
+public class PlaneController extends BaseController<
+        PlaneEntity,
+        PlaneDTO,
+        PlaneRequestDTO,
+        PlaneMapper,
+        PlaneRepository,
+        PlaneSpecification,
+        PlaneService> {
+
+    @Autowired
+    PlaneService planeService;
+
+    @Override
+    protected PlaneService getService() {
+        return planeService;
+    }
+
+}
