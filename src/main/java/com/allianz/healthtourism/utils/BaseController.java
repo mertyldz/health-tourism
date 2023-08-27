990db2769dbae61555359fa37d46f6dbec231c28
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 13:09:21 +0300

    add base controller

---
+++ src/main/java/com/allianz/healthtourism/utils/BaseController.java
@@ -0,0 +1,62 @@
+package com.allianz.healthtourism.utils;
+
+import com.allianz.healthtourism.model.requestDTO.BaseFilterRequestDTO;
+import com.allianz.healthtourism.model.requestDTO.PageDTO;
+import com.allianz.healthtourism.service.BaseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+public abstract class BaseController<
+        Entity extends BaseEntity,
+        DTO extends BaseDTO,
+        RequestDto extends BaseRequestDTO,
+        Mapper extends IBaseMapper<Entity, DTO, RequestDto>,
+        Repository extends IBaseRepository<Entity>,
+        Specification extends BaseSpecification<Entity>,
+        Service extends BaseService<Entity, DTO, RequestDto, Mapper, Repository, Specification>> {
+
+    protected abstract Service getService();
+
+    @PostMapping
+    public ResponseEntity<DTO> save(@RequestBody RequestDto requestDTO) {
+        return new ResponseEntity<>(getService().save(requestDTO), HttpStatus.CREATED);
+    }
+
+    @PostMapping("get-all-filter")
+    public ResponseEntity<PageDTO<DTO>> getAll(@RequestBody BaseFilterRequestDTO baseFilterRequestDTO) {
+        return new ResponseEntity<>(getService().getAll(baseFilterRequestDTO), HttpStatus.OK);
+    }
+
+    @PutMapping("{uuid}")
+    public ResponseEntity<DTO> update(@PathVariable UUID uuid, @RequestBody RequestDto requestDTO) {
+        if (getService().update(uuid, requestDTO) != null) {
+            return new ResponseEntity<>(getService().update(uuid, requestDTO), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("{uuid}")
+    public ResponseEntity<Boolean> deleteByUUID(@PathVariable UUID uuid) {
+        Boolean isDeleted = getService().deleteByUUID(uuid);
+        if (isDeleted) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("{uuid}")
+    public ResponseEntity<DTO> getByUUID(@PathVariable UUID uuid) {
+        DTO dto = getService().getByUUID(uuid);
+        if (dto != null) {
+            return new ResponseEntity<>(dto, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
