9cadcb31469dd45164edadbdf9725ffb7789bd3f
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 13:22:50 +0300

    add hotel service

---
+++ src/main/java/com/allianz/healthtourism/service/HotelService.java
@@ -0,0 +1,45 @@
+package com.allianz.healthtourism.service;
+
+import com.allianz.healthtourism.database.entity.HotelEntity;
+import com.allianz.healthtourism.database.repository.HospitalRepository;
+import com.allianz.healthtourism.database.repository.HotelRepository;
+import com.allianz.healthtourism.database.specification.HospitalSpecification;
+import com.allianz.healthtourism.database.specification.HotelSpecification;
+import com.allianz.healthtourism.mapper.HospitalMapper;
+import com.allianz.healthtourism.mapper.HotelMapper;
+import com.allianz.healthtourism.model.requestDTO.HotelRequestDTO;
+import com.allianz.healthtourism.model.responseDTO.HotelDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HotelService extends BaseService<
+        HotelEntity,
+        HotelDTO,
+        HotelRequestDTO,
+        HotelMapper,
+        HotelRepository,
+        HotelSpecification> {
+
+    @Autowired
+    HotelMapper hotelMapper;
+    @Autowired
+    HotelRepository hotelRepository;
+    @Autowired
+    HotelSpecification hotelSpecification;
+
+    @Override
+    protected HotelMapper getMapper() {
+        return hotelMapper;
+    }
+
+    @Override
+    protected HotelRepository getRepository() {
+        return hotelRepository;
+    }
+
+    @Override
+    protected HotelSpecification getSpecification() {
+        return hotelSpecification;
+    }
+}
