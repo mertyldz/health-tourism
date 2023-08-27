4e643c5cb79ff112d9563014b2726fa6c073aa0b
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 13:25:45 +0300

    add plane service

---
+++ src/main/java/com/allianz/healthtourism/service/PlaneService.java
@@ -0,0 +1,42 @@
+package com.allianz.healthtourism.service;
+
+import com.allianz.healthtourism.database.entity.PlaneEntity;
+import com.allianz.healthtourism.database.repository.PlaneRepository;
+import com.allianz.healthtourism.database.specification.PlaneSpecification;
+import com.allianz.healthtourism.mapper.PlaneMapper;
+import com.allianz.healthtourism.model.requestDTO.PlaneRequestDTO;
+import com.allianz.healthtourism.model.responseDTO.PlaneDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlaneService extends BaseService<
+        PlaneEntity,
+        PlaneDTO,
+        PlaneRequestDTO,
+        PlaneMapper,
+        PlaneRepository,
+        PlaneSpecification> {
+
+    @Autowired
+    PlaneMapper planeMapper;
+    @Autowired
+    PlaneRepository planeRepository;
+    @Autowired
+    PlaneSpecification planeSpecification;
+
+    @Override
+    protected PlaneMapper getMapper() {
+        return planeMapper;
+    }
+
+    @Override
+    protected PlaneRepository getRepository() {
+        return planeRepository;
+    }
+
+    @Override
+    protected PlaneSpecification getSpecification() {
+        return planeSpecification;
+    }
+}
