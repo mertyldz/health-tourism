0e439392c70a379b94ec5f29e542a65d220bc207
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 22:25:09 +0300

    add patient controller

---
+++ src/main/java/com/allianz/healthtourism/controller/PatientController.java
@@ -0,0 +1,34 @@
+package com.allianz.healthtourism.controller;
+
+import com.allianz.healthtourism.database.entity.PatientEntity;
+import com.allianz.healthtourism.database.repository.PatientRepository;
+import com.allianz.healthtourism.database.specification.PatientSpecification;
+import com.allianz.healthtourism.mapper.PatientMapper;
+import com.allianz.healthtourism.model.requestDTO.PatientRequestDTO;
+import com.allianz.healthtourism.model.responseDTO.PatientDTO;
+import com.allianz.healthtourism.service.PatientService;
+import com.allianz.healthtourism.utils.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hospital")
+public class PatientController extends BaseController<
+        PatientEntity,
+        PatientDTO,
+        PatientRequestDTO,
+        PatientMapper,
+        PatientRepository,
+        PatientSpecification,
+        PatientService> {
+
+    @Autowired
+    PatientService patientService;
+
+    @Override
+    protected PatientService getService() {
+        return patientService;
+    }
+
+}
