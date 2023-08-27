4a2e8c9c2f8b44559977a14006b6dc15827517de
Mert Yıldız <yzmert3@gmail.com>
2023-08-27 17:58:32 +0300

    add examination controller

---
+++ src/main/java/com/allianz/healthtourism/controller/ExaminationController.java
@@ -0,0 +1,33 @@
+package com.allianz.healthtourism.controller;
+
+import com.allianz.healthtourism.database.entity.ExaminationEntity;
+import com.allianz.healthtourism.database.repository.ExaminationRepository;
+import com.allianz.healthtourism.database.specification.ExaminationSpecification;
+import com.allianz.healthtourism.mapper.ExaminationMapper;
+import com.allianz.healthtourism.model.requestDTO.ExaminationRequestDTO;
+import com.allianz.healthtourism.model.responseDTO.ExaminationDTO;
+import com.allianz.healthtourism.service.ExaminationService;
+import com.allianz.healthtourism.utils.BaseController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("examination")
+public class ExaminationController extends BaseController<
+        ExaminationEntity,
+        ExaminationDTO,
+        ExaminationRequestDTO,
+        ExaminationMapper,
+        ExaminationRepository,
+        ExaminationSpecification,
+        ExaminationService> {
+
+    @Autowired
+    ExaminationService examinationService;
+
+    @Override
+    protected ExaminationService getService() {
+        return examinationService;
+    }
+}
